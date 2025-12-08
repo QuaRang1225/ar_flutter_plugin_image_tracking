a627a7368f66520d760caa0f9160798c257d7707
QuaRang1225 <dbdudnd1225@gmail.com>
2025-12-08 14:51:04 +0900

    feat: added NodeType cases
    - localUSDZ
    - webGLTF
    - webUSDZ

--- lib/datatypes/node_types.dart
+++ lib/datatypes/node_types.dart
@@ -5,4 +5,7 @@ enum NodeType {
   webGLB, // Node with renderable with fileending .glb loaded from the internet during runtime
   fileSystemAppFolderGLB, // Node with renderable with fileending .glb in the documents folder of the current app
   fileSystemAppFolderGLTF2, // Node with renderable with fileending .gltf in the documents folder of the current app
+  localUSDZ, // Node with renderable with file ending .usdz in the Flutter asset folder
+  webGLTF, // Node with renderable with file ending .gltf in the Flutter asset folder of the web URL
+  webUSDZ // Node with renderable with file ending .gltf in the Flutter asset folder of the web URL
 }
